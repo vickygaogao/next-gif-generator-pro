6c399e7cb05a9a404a1196ce752141f7a02031bc
gaoying <gaoying@9fbank.cc>
2024-10-29 09:57:27 +0800

    first commit

--- next.config.ts
+++ next.config.ts
@@ -2,6 +2,13 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
+  compress: true,
+  images: {
+    unoptimized: false,
+  },
+  compiler: {
+    removeConsole: process.env.NODE_ENV === "production",
+  },
 };
 
 export default nextConfig;
